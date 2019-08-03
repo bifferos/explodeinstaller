f6c479372e96bb69805fffd6757eefce1a94770e
bifferos <bifferos@gmail.com>
2019-08-03 23:14:21 +0100

    Pull the VM creation into another script

---
+++ examples/serial_install/makevm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VM_NAME=slack_test
+VBOX_SOCKET="/tmp/vbox"
+
+VBoxManage controlvm $VM_NAME poweroff
+VBoxManage unregistervm $VM_NAME --delete
+
+VBoxManage createvm --name $VM_NAME --register
+VBoxManage modifyvm $VM_NAME --memory 512
+VBoxManage modifyvm $VM_NAME --nic1 nat
+VBoxManage modifyvm $VM_NAME --boot1 dvd
+VBoxManage storagectl $VM_NAME --name IDE --add ide
+VBoxManage createmedium disk --filename disk.vdi --size 20480
+VBoxManage storageattach $VM_NAME --storagectl IDE --port 0 --device 0 --type hdd --medium disk.vdi
+VBoxManage storageattach $VM_NAME --storagectl IDE --port 1 --device 0 --type dvddrive --medium generated.iso
+VBoxManage modifyvm $VM_NAME --uart1 0x3f8 4 --uartmode1 server $VBOX_SOCKET
